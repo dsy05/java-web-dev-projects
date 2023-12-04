383dda8e5d550464e3935f9185815ec913ed98df
Daniyal Syed <daniyal.syed@live.com>
2023-12-03 19:13:23 -0600

    ch2 studio

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,2 +1,13 @@
-package org.launchcode;public class Area {
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter circle radius: ");
+        Double radius = input.nextDouble();
+        Double areaCircle = Circle.getArea(radius);
+        System.out.println("Area of a circle of radius " + radius + " is: " + areaCircle);
+    }
 }
