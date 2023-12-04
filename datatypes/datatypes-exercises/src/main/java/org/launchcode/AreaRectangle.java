527a122494ceb8e303903911ce4a3c34b1238846
Daniyal Syed <daniyal.syed@live.com>
2023-12-03 19:11:43 -0600

    ch2 exercises

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/AreaRectangle.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/AreaRectangle.java
@@ -1,2 +1,13 @@
-package org.launchcode;public class AreaRectangle {
+package org.launchcode;
+
+import java.util.Scanner;
+public class AreaRectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter length of the rectangle:");
+        Double length = input.nextDouble();
+        System.out.println("Enter width of the rectangle:");
+        Double width = input.nextDouble();
+        System.out.println("Rectangle area: " + (length * width));
+    }
 }
--- datatypes/datatypes-exercises/src/main/java/org/launchcode/GasMileageCalc.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/GasMileageCalc.java
@@ -1,2 +1,13 @@
-package org.launchcode;public class GasMileageCalc {
+package org.launchcode;
+
+import java.util.Scanner;
+public class GasMileageCalc {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of miles driven:");
+        Double miles = input.nextDouble();
+        System.out.println("Enter amount of gas consumed:");
+        Double gasUsed = input.nextDouble();
+        System.out.println("Gas Mileage: " + (miles / gasUsed) + "miles-per-gallon");
+    }
 }
