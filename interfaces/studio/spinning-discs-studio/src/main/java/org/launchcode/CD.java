8b7a1f668930896b5f12fa0a1381163c98e6fb57
Daniyal Syed <daniyal.syed@live.com>
2024-01-04 17:51:54 -0600

    chapter 8 studio

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
@@ -1,6 +1,84 @@
 package org.launchcode;
 
-public class CD {
+public class CD extends Media implements Rewritable{
+
+    private boolean isMusicCD;
+
+    public CD(String name, boolean isMusicCD) {
+        super(name, "", 800, 700);
+        this.isMusicCD = isMusicCD;
+        setDiscType(isMusicCD ? "Music CD" : "CD-RW");
+    }
+
+    public CD(String name) {
+        this(name, false);
+    }
+
+    public boolean isMusicCD() {
+        return isMusicCD;
+    }
+
+    public void setMusicCD(boolean musicCD) {
+        isMusicCD = musicCD;
+    }
+
+    @Override
+    public String toString() {
+        String header = isMusicCD ? "Tracks" : "Files";
+        return super.toString() + getFormattedFileList(header);
+    }
+
+    //methods required by Rewriteable
+
+    @Override
+    public void writeFile(File file) {
+        spinDisc();
+        String fileType = isMusicCD ? "Track" : "File";
+        if (getFiles().contains(file)) {
+            System.out.println("The " + fileType + " " + file.getName() + " has already been added.");
+        } else if (getSpaceUsed() + file.getSize() > getCapacity()) {
+            System.out.println("Warning: There is not enough space on the " + getDiscType() + " for " + file.getName() + ".");
+        } else {
+            getFiles().add(file);
+            System.out.println("The " + fileType + " " + file.getName() + " has been added to " + getName() + ".");
+        }
+    }
+
+    @Override
+
+    public void removeFile(File file) {
+        String newLine = System.lineSeparator();
+        if (isMusicCD) {
+            System.out.println(newLine + "Individual file cannot be removed form a music CD once written.");
+        } else {
+            spinDisc();
+            if (fileIsPresent(file)) {
+                getFiles().remove(file);
+                System.out.println("The file " + file.getName() + " has been removed from the " + getDiscType());
+            }
+        }
+    }
+
+    @Override
+    public void reformatDisc() {
+        spinDisc();
+        getFiles().clear();
+        setMusicCD(false);
+        setDiscType("CD-RW");
+        setName("Unnamed CD-RW");
+    }
+
+    @Override
+    public void runFile(File file) {
+        if (fileIsPresent(file)) {
+            spinDisc();
+            String verb = isMusicCD ? "Playing " : "Opening File ";
+            System.out.println(verb + file.getName() + "...");
+        }
+
+    }
+
+
     // TODO: Implement your custom interface.
 
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -4,7 +4,72 @@ public class Main {
     public static void main(String[] args) {
 
         // TODO: Declare and initialize a CD and a DVD object.
-
         // TODO: Call each CD and DVD method to verify that they work as expected.
+
+        CD greatestHits = new CD("The Greatest Hits", true);
+        File sound = new File("Sound", 60);
+        File harmonic = new File("Harmonic", 75);
+        File techno = new File("Techno", 150);
+        greatestHits.writeFile(sound);
+        greatestHits.writeFile(harmonic);
+        greatestHits.writeFile(techno);
+
+        CD houseProject = new CD("House Project");
+        File kitchenDesign = new File("Kitchen Design", 200);
+        File fireplace = new File("Fireplace", 100);
+        File masterBath = new File("Master Bath", 50);
+        houseProject.writeFile(kitchenDesign);
+        houseProject.writeFile(fireplace);
+        houseProject.writeFile(masterBath);
+
+        DVD fridayBingeNight = new DVD("Friday Movie Night");
+        File heavyweights = new File("Heavyweights", 300);
+        File batman = new File("Batman", 350);
+        File homeAlone = new File("Home Alone", 325);
+        fridayBingeNight.writeFile(heavyweights);
+        fridayBingeNight.writeFile(batman);
+        fridayBingeNight.writeFile(homeAlone);
+
+        //Print each CD and DVD object
+        System.out.println(greatestHits);
+        System.out.println(houseProject);
+        System.out.println(fridayBingeNight);
+
+        //use run file on CD's and DVD
+        greatestHits.runFile(sound);
+        houseProject.runFile(masterBath);
+        fridayBingeNight.runFile(batman);
+
+        //writing file already of CD's and DVD
+        greatestHits.writeFile(sound);
+        houseProject.writeFile(masterBath);
+        fridayBingeNight.writeFile(heavyweights);
+
+        //use removefile() to remove files from CD's and DVD and then try running the file
+        greatestHits.removeFile(sound);
+        greatestHits.runFile(sound);
+
+        houseProject.removeFile(masterBath);
+        houseProject.runFile(masterBath);
+
+        fridayBingeNight.removeFile(heavyweights);
+        fridayBingeNight.runFile(heavyweights);
+
+        //use reformatDisc() to erase all files the try running a file
+        greatestHits.reformatDisc();
+        greatestHits.runFile(harmonic);
+
+        houseProject.reformatDisc();
+        houseProject.runFile(kitchenDesign);
+
+        fridayBingeNight.reformatDisc();
+        fridayBingeNight.runFile(batman);
+
+        //create a file greater than capacity of media
+        File largeMusicFile = new File("Larger Than Capacity", 900);
+        greatestHits.writeFile(largeMusicFile);
+
+        File largeVideoFile = new File("Larger Than Capacity UltraHD", 7000);
+        fridayBingeNight.writeFile(largeVideoFile);
     }
 }
