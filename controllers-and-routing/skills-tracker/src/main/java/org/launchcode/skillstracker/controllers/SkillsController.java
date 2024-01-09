a0b7887092ca3948e360af21ae79dc2d8c8cc3a1
Daniyal Syed <daniyal.syed@live.com>
2024-01-08 20:24:53 -0600

    skills tracker

---
+++ controllers-and-routing/skills-tracker/src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -0,0 +1,37 @@
+package org.launchcode.skillstracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class SkillsController {
+
+    @GetMapping("/")
+    public String displayHomePage() {
+        return "<html>" +
+                "<body>" +
+                "<h1>Skills Tracker</h1>" +
+                "<h2>We have a few skills we would like to learn. Here is the list!</h2>" +
+                "<ol>" +
+                "<li>Java</li>" +
+                "<li>JavaScript</li>" +
+                "<li>Python</li>" +
+                "</ol>" +
+                "</body>" +
+                "</html>";
+    }
+
+    @GetMapping("forms")
+    public String displayForms() {
+        return "<html>"
+                "<form method='post'>" +
+                "<"
+    }
+
+
+
+
+
+}
