7171783ead6fc26a5ce01a4bba9de900dd564910
Daniyal Syed <daniyal.syed@live.com>
2024-01-28 00:37:38 -0600

    exceptions exercises first part

---
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/DivideZeroException.java
@@ -0,0 +1,8 @@
+package org.launchcode;
+
+public class DivideZeroException extends ArithmeticException{
+        public DivideZeroException(String message) {
+            super(message);
+        }
+
+}
--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,10 +1,23 @@
 package org.launchcode;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        int x;
+        int y;
+        Scanner input;
+
+        input = new Scanner(System.in);
+        System.out.println("Enter a value for x(numerator): ");
+        x = input.nextInt();
+        System.out.println("Enter a value for y(denominator): ");
+        y = input.nextInt();
+        input.close();
+
+        Divide(x,y);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -15,13 +28,21 @@ public class Main {
         // Test out your CheckFileExtension() function!
     }
 
-    public static void Divide(int x, int y)
-    {
+    public static void Divide(int x, int y) {
+        if (y == 0) {
+            try {
+                throw new DivideZeroException("Cannot divide by zero! Y value cannot equal 0!");
+            } catch (DivideZeroException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("The quotient for: " + x + " / " + y + " = " + x/y);
         // Write code here!
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
-    }
+//    public static int CheckFileExtension(String fileName)
+//    {
+//        // Write code here!
+//    }
 }
